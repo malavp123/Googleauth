425da24e00896f002b39a30cccfd6ae58d1d5fe2
malavp123 <malavparekh123@gmail.com>
2019-01-22 14:36:20 +0530

    Add files via upload

---
+++ GoogleAuth.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+var (
+	googleOauthConfig = &oauth2.Config{
+		RedirectURL:  "http://localhost:8080/callback",
+		ClientID:     "870538702250-j9g8k61he7d0t5uasejmssavsmji44gj.apps.googleusercontent.com",
+		ClientSecret: "BvTt6DER1leOeXWKs11kCYi9",
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+		Endpoint:     google.Endpoint,
+	}
+	randomState = "random"
+)
+
+func main() {
+	http.HandleFunc("/", handleHome)
+	http.HandleFunc("/login", handleLogin)
+	http.HandleFunc("/callback", handleCallBack)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	var html = `<html><body><a href="/login">Login with Google</a></body></html>`
+	fmt.Fprint(w, html)
+
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	url := googleOauthConfig.AuthCodeURL(randomState)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+
+}
+
+func handleCallBack(w http.ResponseWriter, r *http.Request) {
+	if r.FormValue("state") != randomState {
+		fmt.Println("state is not valid")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	token, err := googleOauthConfig.Exchange(oauth2.NoContext, r.FormValue("Code"))
+	if err != nil {
+		fmt.Printf("Could not get token %s \n", err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	resp, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token" + token.AccessToken)
+	if err != nil {
+		fmt.Printf("Could not create get request %s \n", err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	defer resp.Body.Close()
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Could not parse response %s \n", err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	fmt.Fprintf(w, "Response: %s", content)
+}
